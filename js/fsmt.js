168916086ba30bafda5a4bc9403bf7b3b1deaf58
CCCM IMO TEAM <cccmiraq@gmail.com>
2017-11-06 10:11:54 +0300

    Update fsmt.js

--- js/fsmt.js
+++ js/fsmt.js
@@ -19,7 +19,7 @@ jQuery(document).ready(function() {
 			}
 			else{}
 */
-	var u = "https://dl.dropbox.com/s/icxwgskzcknabj6/fsmt_sites.csv?dl=0";
+	var u = "https://dl.dropbox.com/s/wy4zvtmvkgc/fsmt_sites.csv?dl=0";
 	var map, loc, name;
 	d3.csv(u, function(data) {
 		console.log(data)
